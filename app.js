5cde0edf2e3d4424835b1227b41b3534895dad31
Aditya Suryawanshi <97177344+adityasurya4103@users.noreply.github.com>
2023-09-14 19:17:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+// Sticky navbar
+let header = document.querySelector('header');
+
+// header.classList.toggle('sticky', window.scrollY > 100); // Changed 'screenY' to 'scrollY'
+
+    // function Toggle() {
+    //   const navbar = document.getElementById("nav");
+    //   // navbar.style.display="block";
+    //   console.log('clicked');
+    //   if(navbar.style.display==="block"){
+    //     navbar.style.display="none";
+    //   }
+    //   else{
+    //     navbar.style.display="block";
+    //   }
+
+    // }
