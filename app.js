9875197b934933fa52a159a9ef206997ce362743
Abhishek Paturkar <96946118+abhishekpaturkar@users.noreply.github.com>
2023-09-16 21:20:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-// Sticky navbar
-let header = document.querySelector('header');
+function Toggle() {
+    const navbar = document.getElementById("nav");
+    // navbar.style.display="block";
+    console.log('clicked');
+    if (navbar.style.display === "block") {
+        navbar.style.display = "none";
+    }
+    else {
+        navbar.style.display = "block";
+    }
 
-// header.classList.toggle('sticky', window.scrollY > 100); // Changed 'screenY' to 'scrollY'
+}
 
-    // function Toggle() {
-    //   const navbar = document.getElementById("nav");
-    //   // navbar.style.display="block";
-    //   console.log('clicked');
-    //   if(navbar.style.display==="block"){
-    //     navbar.style.display="none";
-    //   }
-    //   else{
-    //     navbar.style.display="block";
-    //   }
 
-    // }
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true,
+    },
+    loop: true,
+  });
+
